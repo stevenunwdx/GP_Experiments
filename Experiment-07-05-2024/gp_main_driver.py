e4f219e027d062e846b6fdf095ae742de133b6f5
stevenunwdx <unsteven78@gmail.com>
2024-07-05 15:36:56 -0500

    Add files via upload

---
+++ Experiment-07-05-2024/gp_main_driver.py
@@ -0,0 +1,170 @@
+import gp_models
+import gp_utility_functions
+import test_functions
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+
+print('Gaussian Process Experiment Driver')
+print('Synclesis, Inc\n')
+
+print('Gaussian Process Experiment 1:\n')
+print('-One GP Model (RBF Kernel, Constant Mean) tested on each Test Function 0,1,2,3,4')
+print('-Noiseless random training data uniformly distributed on rectangular grid')
+print('-Noiseless test data on uniform rectangular grids of 625 points (625 = 25^2 and 625 = 5^4)')
+print('-Each instance of each model trained in 500 epochs (hyperparameter optimization)')
+print('-Experiment data consist of four items:')
+print('(1.) Raw CSV file')
+print('(2.) MS Excel file')
+print('(3.) Model evaluation graph: R^2 score vs. number of training samples')
+print('(4.) Model evaluation graph: MAPE vs. number of training samples\n')
+print('All experiment data items are created and saved to the local directory of this script.\n')
+print('______________________Begin Experiment________________________')
+
+modelclass = gp_models.BasicGPRegression
+model_list = [modelclass]
+
+func_list = [
+             test_functions.test_function_0,
+             test_functions.test_function_1,
+             test_functions.test_function_2,
+             test_functions.test_function_3,
+             test_functions.test_function_4
+             ]
+numbers_samples = list(range(5,200,5))
+#numbers_samples = [5,10]
+print('Number of learning problems:',len(func_list))
+print('Numbers of sample sizes: ',len(numbers_samples))
+print('Number of models:',len(model_list),'\n')
+
+print('Total Number of Model Instances in this Experiment:',len(func_list)*len(numbers_samples)*len(model_list))
+
+experiment_dict = {'Test Function':[],
+                   'Model':[],
+                   'Number of Samples':[],
+                   'Coefficient of Determination':[],
+                   'MAPE':[],
+                   'Max Residual Error':[]}  #dictionary is a "3-d" array indexing experiments and outcomes
+
+for k, func in enumerate(func_list):  #each learning problem
+    for n in numbers_samples: #each number of samples
+        if (k< len(func_list)-1):
+            train_input, train_output = gp_utility_functions.generate_data_2d_problem(func,0,30,0,30,data_set_size=n,save=True)
+            test_input, test_output = gp_utility_functions.generate_data_2d_grid(func,0,30,0,30,25)
+        else:
+            train_input, train_output = gp_utility_functions.generate_data_4d_problem(func,1,6,1,6,1,6,1,6,data_set_size=n,save=True)
+            test_input, test_output = gp_utility_functions.generate_data_4d_grid(func,1,6,1,6,1,6,1,6,5)
+        for i, modelclass in enumerate(model_list):
+            modelname = 'testfunction{}_model{}_NSamples={}'.format(k,modelclass.__name__,n)
+            gp_utility_functions.single_instance_model_training(modelclass,modelname,train_input,train_output,n_epochs=500)
+
+            r_squared_score, mape, max_res_error = gp_utility_functions.single_instance_model_testing(modelclass,modelname,test_input,test_output)
+
+            experiment_dict['Test Function'].append(k)
+            experiment_dict['Model'].append(modelclass.__name__)
+            experiment_dict['Number of Samples'].append(n)
+            experiment_dict['Coefficient of Determination'].append(r_squared_score)
+            experiment_dict['MAPE'].append(mape)
+            experiment_dict['Max Residual Error'].append(max_res_error)
+experiment_data_frame = pd.DataFrame(experiment_dict)
+output_file_name = 'GP_Experiment_1_Data'
+experiment_data_frame.to_csv(output_file_name,index =False)
+experiment_data_frame.to_excel('GP_Experiment_1_Data.xlsx')
+
+#plot array of subplots for R^2 Score vs. NSamples.
+
+fig, ax = plt.subplots(len(model_list),len(func_list),figsize = (20,5))
+
+for k in range(len(func_list)):
+    for i, modelclass in enumerate(model_list):
+        mask = ((experiment_data_frame['Test Function'] == k) & (experiment_data_frame['Model']== modelclass.__name__))
+        df_test_function_k_model_i = experiment_data_frame[mask]
+        ns_samples = df_test_function_k_model_i['Number of Samples'].to_numpy()
+        r_squared_scores = df_test_function_k_model_i['Coefficient of Determination'].to_numpy()
+        if (len(model_list)==1):
+            ax[k].plot(ns_samples,r_squared_scores,'o-',linewidth=0.7, markersize=2,label = 'R^2 Score')
+            ax[k].set_xlim(0,max(numbers_samples))
+            ax[k].set_ylim(0,1.2)
+            ax[k].hlines(y=0.99,xmin=0, xmax=max(numbers_samples),linestyle='--',linewidth=0.5,color ='r',label = 'R^2 Score = 0.99')
+            ax[k].set_xlabel('Number of Training Samples')
+            ax[k].set_ylabel('Coefficient of Determination')
+            ax[k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[k].legend()
+
+        else: 
+            print('else block executed: multiple models')
+            ax[i,k].plot(ns_samples,r_squared_scores,'o-',linewidth=0.7, markersize=3,label = 'R^2 Score')
+            ax[i,k].set_xlim(0,max(numbers_samples))
+            ax[i,k].set_ylim(0,1.2)
+            ax[i,k].hlines(y=0.99,xmin=0, xmax=max(numbers_samples),linestyle='--',linewidth=0.5,color ='r',label = 'R^2 Score = 0.99')
+            ax[i,k].set_xlabel('Number of Training Samples')
+            ax[i,k].set_ylabel('Coefficient of Determination')
+            ax[i,k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[i,k].legend()
+
+fig.savefig('GP_Experiment_1_R2_Graph')
+
+#plot array of subplots for MAPE vs. NSamples.
+
+fig, ax = plt.subplots(len(model_list),len(func_list),figsize = (20,5))
+
+for k in range(len(func_list)):
+    for i, modelclass in enumerate(model_list):
+        mask = ((experiment_data_frame['Test Function'] == k) & (experiment_data_frame['Model']== modelclass.__name__))
+        df_test_function_k_model_i = experiment_data_frame[mask]
+        ns_samples = df_test_function_k_model_i['Number of Samples'].to_numpy()
+        mapes = df_test_function_k_model_i['MAPE'].to_numpy()
+        if (len(model_list)==1):
+            ax[k].plot(ns_samples,mapes,'o-',linewidth=0.7, markersize=2,label = 'MAPE')
+            ax[k].set_xlim(0,max(numbers_samples))
+            ax[k].set_xlabel('Number of Training Samples')
+            ax[k].set_ylabel('MAPE')
+            ax[k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[k].legend()
+
+        else: 
+            print('else block executed: multiple models')
+            ax[i,k].plot(ns_samples,mapes,'o-',linewidth=0.7, markersize=3,label = 'MAPE')
+            ax[i,k].set_xlim(0,max(numbers_samples))
+            ax[i,k].set_xlabel('Number of Training Samples')
+            ax[i,k].set_ylabel('MAPE')
+            ax[i,k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[i,k].legend()
+
+fig.savefig('GP_Experiment_1_MAPE_Graph')
+
+#plot array of subplots for Maximum Residual Error vs. NSamples.
+
+fig, ax = plt.subplots(len(model_list),len(func_list),figsize = (20,5))
+
+for k in range(len(func_list)):
+    for i, modelclass in enumerate(model_list):
+        mask = ((experiment_data_frame['Test Function'] == k) & (experiment_data_frame['Model']== modelclass.__name__))
+        df_test_function_k_model_i = experiment_data_frame[mask]
+        ns_samples = df_test_function_k_model_i['Number of Samples'].to_numpy()
+        max_res_errors = df_test_function_k_model_i['Max Residual Error'].to_numpy()
+        if (len(model_list)==1):
+            ax[k].plot(ns_samples,max_res_errors,'o-',linewidth=0.7, markersize=2,label = 'Maximum Residual Error')
+            ax[k].set_xlim(0,max(numbers_samples))
+            ax[k].set_xlabel('Number of Training Samples')
+            ax[k].set_ylabel('Max Residual Error')
+            ax[k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[k].legend()
+
+        else: 
+            print('else block executed: multiple models')
+            ax[i,k].plot(ns_samples,max_res_errors,'o-',linewidth=0.7, markersize=3,label = 'Maximum Residual Error')
+            ax[i,k].set_xlim(0,max(numbers_samples))
+            ax[i,k].set_xlabel('Number of Training Samples')
+            ax[i,k].set_ylabel('Max Residual Error')
+            ax[i,k].set_title(f'Test Function {k}\nModel: {modelclass.__name__}')
+            ax[i,k].legend()
+
+fig.savefig('GP_Experiment_1_Max_Res_Error_Graph')
+
+print('__________________Program run successful: experiment complete___________________')
+
+
+
---
+++ Experiment-07-05-2024/test_functions.py
@@ -0,0 +1,97 @@
+import numpy as np
+#import math
+#import pandas as pd
+#import torch
+#Test Function 0
+
+def test_function_0(X,Y):
+    value_array = X**2 -Y**2  #no meshgrid in function implementation.
+    return value_array
+
+#Test Function 1
+
+def modified_ackley(X,Y):
+    value_array = - 5 * np.exp(-0.2* np.sqrt(0.5*((0.5*(X-20))**2 + (0.5*(Y-20))**2) ) ) - np.exp(0.5* (np.cos(0.125*(X-20)) + np.cos(0.125*(Y-20))  ) ) +np.exp(1)
+    return value_array
+
+def step_function(X,Y):
+    value_array = 0.5*( np.exp(-0.5*(np.floor(0.2*X))) + np.exp(-0.5*(np.floor(0.2*Y))) ) - 1
+    return value_array
+
+def indicator_square(X,Y):
+    if (len(list(X.shape))==1):
+        X=np.expand_dims(X,axis=0)
+    if (len(list(Y.shape))==1):
+        Y=np.expand_dims(Y,axis=0)
+    xdim = X.shape[0]
+    ydim = X.shape[1]
+    value_array = np.zeros((xdim,ydim))
+    for idx1 in range(xdim):
+        for  idx2 in range(ydim):
+                    if ( ((X[idx1][idx2] >= 10) & (X[idx1][idx2]<=30)) & ((Y[idx1][idx2] >=10)& (Y[idx1][idx2]<=30)) ):
+                        value_array[idx1,idx2] = 1
+    return value_array
+
+def indicator_edges(X,Y):
+    if (len(list(X.shape))==1):
+        X=np.expand_dims(X,axis=0)
+    if (len(list(Y.shape))==1):
+        Y=np.expand_dims(Y,axis=0)
+    xdim = X.shape[0]
+    ydim = X.shape[1]
+    value_array = np.ones((xdim,ydim))
+    for idx1 in range(xdim):
+        for  idx2 in range(ydim):
+            if ( ((X[idx1][idx2] >= 10) & (X[idx1][idx2]<=30)) & ((Y[idx1][idx2] >=10)& (Y[idx1][idx2]<=30)) ):
+                value_array[idx1,idx2] = 0
+    return value_array
+
+def test_function_1(X,Y):
+    value_array = modified_ackley(X,Y) * indicator_square(X,Y) + step_function(X,Y) * indicator_edges(X,Y)
+    return value_array
+
+
+#Test Function 2
+
+def test_function_2(X,Y):
+    R_squared = X**2+ Y**2
+    value_array = (1/(X**2-9)) * np.exp(-0.1*(R_squared))*np.sin(R_squared)
+    return value_array
+
+#Test Function 3
+
+def test_function_3(X,Y,a=5,b=5):
+    if (len(list(X.shape))==1):
+        X=np.expand_dims(X,axis=0)
+    if (len(list(Y.shape))==1):
+        Y=np.expand_dims(Y,axis=0)
+    ydim = X.shape[0]
+    xdim = X.shape[1]
+    value_array = np.zeros((ydim,xdim))
+    for idx1 in range(ydim):
+        for idx2 in range(xdim):
+            x = X[idx1][idx2]
+            y = Y[idx1][idx2]
+            if abs(x-y)<1:
+                value_array[idx1][idx2] += a*np.exp(-1/( 1 - (x-y)**2 )  )
+            if abs(-x-y)<1:
+                value_array[idx1][idx2] += b* np.exp(-1/( 1 - (-x-y)**2 )  ) 
+    value_array = value_array + 1
+    return value_array
+
+#Test Function 4
+
+def test_function_4(X1,X2,X3,X4):
+    Rsquared = X1**2 + X2**2 + X3**2 + X4**2
+    value_array = 100/( (X1**2 + X2**2) * (X3**2 + X4**2) ) * (X1**2) * X2 * np.exp(-0.1*Rsquared)
+    return value_array
+#Some sections of Test Function 4
+def test_function_4_x1x2_section(x1,x2,X3,X4):
+    value_array = test_function_4(x1,x2,X3,X4)  
+    value_array = value_array[0,0,:,:]
+    return value_array
+
+def test_function_4_x3x4_section(X1,X2,x3,x4):
+    value_array = test_function_4(X1,X2,x3,x4)
+    value_array = value_array[:,:,0,0]
+    return value_array
